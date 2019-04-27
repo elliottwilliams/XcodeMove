158e6c35cd74e8534371db6fbfb3cd7d2a16953b
Elliott Williams <e@elliottwillia.ms>
2019-04-27 15:26:22 -0700

    Move specs into module namespace

--- spec/xcmv_spec.rb
+++ spec/xcmv_spec.rb
@@ -2,76 +2,78 @@ require 'xcodeproj'
 require 'xcmv'
 require 'pathname'
 
-describe XcodeMove do
-  let(:options) do 
-    {targets: ["a", "b"], headers: [XcodeMove::HeaderVisibility::PUBLIC]}
-  end
-
-  around do |ex|
-    Dir.mktmpdir do |dir|
-      dir = Pathname.new(dir)
-      (dir/"a").mkdir
-      (dir/"a/a.swift").write ""
-      (dir/"b").mkdir
-      (dir/"b/b.swift").write ""
-      (dir/"main.swift").write ""
-      Dir.chdir(dir) { ex.run }
-    end
-  end
-
-  describe '::mv' do
-    before(:example) do
-      expect(subject).to receive(:puts)
-      expect(subject).to receive(:disk_mv)
-      expect(subject).to receive(:save)
+module XcodeMove 
+  describe self do
+    let(:options) do 
+      {targets: ["a", "b"], headers: [HeaderVisibility::PUBLIC]}
     end
 
-    it 'moves a file to a destination path' do
-      # xcmv a/a.swift a/aa.swift
-      src = Pathname.new("a/a.swift")
-      dst = Pathname.new("a/aa.swift")
-      expect(subject).to receive(:project_mv).with(
-        XcodeMove::File.new(src), XcodeMove::File.new(dst), options
-      )
-
-      subject.mv(src, dst, options)
-    end
-
-    it 'moves a file into an existing directory' do
-      # xcmv a/a.swift b
-      src = Pathname.new("a/a.swift")
-      dst = Pathname.new("b/a.swift")
-      expect(subject).to receive(:project_mv).with(
-        XcodeMove::File.new(src), XcodeMove::File.new(dst), options
-      )
-
-      subject.mv(src, dst, options)
-    end
-
-    xit 'moves a directory to a destination path' do
-      # xcmv a c
-      src = Pathname.new("a")
-      dst = Pathname.new("c")
-      expect(subject).to receive(:project_mv).with(
-        XcodeMove::Group.new(src), XcodeMove::Group.new(dst), options
-      )
-
-      subject.mv(src, dst, options)
-    end
-
-    it 'does not move a directory onto an existing file' 
-  end
-
-  describe '::project_mv' do
-    context 'moving a file' do
-      subject(:src) { XcodeMove::File.new "a/a.swift" }
-      subject(:dst) { XcodeMove::File.new "a/aa.swift" }
-
-      it 'removes src file and creates dst' do
-         
+    around do |ex|
+      Dir.mktmpdir do |dir|
+        dir = Pathname.new(dir)
+        (dir/"a").mkdir
+        (dir/"a/a.swift").write ""
+        (dir/"b").mkdir
+        (dir/"b/b.swift").write ""
+        (dir/"main.swift").write ""
+        Dir.chdir(dir) { ex.run }
       end
     end
 
-    context 'moving a directory'
+    describe '::mv' do
+      before(:example) do
+        expect(subject).to receive(:puts)
+        expect(subject).to receive(:disk_mv)
+        expect(subject).to receive(:save)
+      end
+
+      it 'moves a file to a destination path' do
+        # xcmv a/a.swift a/aa.swift
+        src = Pathname.new("a/a.swift")
+        dst = Pathname.new("a/aa.swift")
+        expect(subject).to receive(:project_mv).with(
+          File.new(src), File.new(dst), options
+        )
+
+        subject.mv(src, dst, options)
+      end
+
+      it 'moves a file into an existing directory' do
+        # xcmv a/a.swift b
+        src = Pathname.new("a/a.swift")
+        dst = Pathname.new("b/a.swift")
+        expect(subject).to receive(:project_mv).with(
+          File.new(src), File.new(dst), options
+        )
+
+        subject.mv(src, dst, options)
+      end
+
+      xit 'moves a directory to a destination path' do
+        # xcmv a c
+        src = Pathname.new("a")
+        dst = Pathname.new("c")
+        expect(subject).to receive(:project_mv).with(
+          Group.new(src), Group.new(dst), options
+        )
+
+        subject.mv(src, dst, options)
+      end
+
+      it 'does not move a directory onto an existing file' 
+    end
+
+    describe '::project_mv' do
+      context 'moving a file' do
+        subject(:src) { File.new "a/a.swift" }
+        subject(:dst) { File.new "a/aa.swift" }
+
+        it 'removes src file and creates dst' do
+           
+        end
+      end
+
+      context 'moving a directory'
+    end
   end
 end
